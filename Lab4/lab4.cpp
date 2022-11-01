2288c77f7da43c8b9fb1ce41f338b6094d3d3c12
Nathan Jaggers <nathanj8910@gmail.com>
2022-11-01 00:09:08 -0700

    I think it is closer.

--- Lab4/lab4.cpp
+++ Lab4/lab4.cpp
@@ -13,20 +13,33 @@
 using namespace cv;
 using namespace std;
 
+//defines for any values you want to change easily
 #define THREADCOUNT 1
 
-//Create threads
-pthread_t thread[THREADCOUNT]
+//Create threads, and related pointers
+pthread_t thread[THREADCOUNT];
 
+void *threadStatus;
+
+//set thread attributes
+//pthread_attr_t attr;
+//pthread_attr_init(&attr);
+//pthread_attr_setdetachstate(&attr,  PTHREAD_CREATE_JOINABLE);
+
+//create barriers for threads
+pthread_barrier_t mainBarrier, threadBarrier;
+
+//create struct to hold arguments to pass to thread function
 struct threadArgs {
-    Mat input;
-    Mat output;
-    int thread;
-}
+    Mat *input;
+    Mat *output;
+    int start1, stop1, start2, stop2;
+};
 
-Mat to442_grayscale(Mat);
-Mat to442_sobel(Mat);
-void *grobel(void *threadArgs);
+//prototypes for functions
+void to442_grayscale(Mat*, Mat*, int, int);
+void to442_sobel(Mat*, Mat*, int, int);
+void *grobel(void *args);
 
 int main(int argc, char *argv[])
 {
@@ -50,26 +63,52 @@ int main(int argc, char *argv[])
  		return -1;
  	}
 	
-	//Create Mat and read in one frame at a time from video
-	Mat frame, sobelimage;
+	//Create Mats and read in one frame from video
+	Mat frame; 
+    cap.read(frame);
+    Mat sobelimage(frame.rows-2, frame.cols-2, CV_8UC1);
 
-	//Create threads
-    //pthread_t thread[THREADCOUNT]
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr,  PTHREAD_CREATE_JOINABLE);
-    //prob will need to change to array of thread args
-    //or will need to hard code for either 1 or 4/5 threads
-    struct threadArgs single_t =  {.input  =  frame,  .output  =  sobelimage, thread = 1};
+    //determine chunck size for threads
+    int graychunk = (frame.rows*frame.cols)/THREADCOUNT;
+    int sobelchunk = ((frame.rows-2)*(frame.cols-2))/THREADCOUNT;
+
+    //create and initialize arguments for threaded functions
+    struct threadArgs multi_t[THREADCOUNT]; 
+    
+    for(int i = 0; i < THREADCOUNT; i++)
+    {
+        multi_t[i].input  = &frame;
+        multi_t[i].output = &sobelimage;
+        multi_t[i].start1 = i*graychunk;
+        multi_t[i].stop1  = i*graychunk + graychunk - 1;
+        multi_t[i].start2 = i*sobelchunk;
+        multi_t[i].stop2  = i*sobelchunk + sobelchunk - 1;
+    }  
+
    
-    //can create threads and join them in while loop each time through
-    //can create persistant threads outside of loop, and have barriers that sync 
-    //main and frame threads between image processing and image show
+    //create barrier for main thread
+    pthread_barrier_init(&mainBarrier, NULL, THREADCOUNT + 1); 
+    
+    //create persistent threads
 
-    while(cap.read(frame))
+    for(int i = 0; i < THREADCOUNT; i++)
+    {
+        pthread_create(&thread[i], NULL, grobel, (void *)(&multi_t[i]));
+    }
+
+    //wait for processing of first frame
+    //pthread_barrier_wait(&mainBarrier);
+
+    //while(cap.read(frame))
+    while(true)
 	{
-            //take frame, apply grayscale and sobel through threads
+            //grobel(multi_t);    
+
+            //after grabbing new frame, wait until processing is finished 
+            //pthread_barrier_wait(&mainBarrier);
             
+            cap >>frame;
+            if(frame.empty()){break;}
 
             //show final image
         	imshow("Sobel", sobelimage);
@@ -79,43 +118,49 @@ int main(int argc, char *argv[])
         	{ 
             		break; 
         	}
+
+            //wait for image to show and ensure esc is not pressed
+            //pthread_barrier_wait(&mainBarrier);
 	}
 
     //Join threads
+    for(int i = 0; i < THREADCOUNT; i++)
+    {
+        pthread_join(thread[i], &threadStatus);
+    }
+
 
 	return 0;
 }
 
-Mat to442_grayscale(Mat imcolor)
+void to442_grayscale(Mat *imcolor, Mat *imgray, int start, int stop)
 {
-	//Define new matrix with one channel
-	Mat imgray(imcolor.rows, imcolor.cols, CV_8UC1);
-	
+
 	//using pixels from color image, generate grayscale
-	uchar *inPixels = (uchar*) imcolor.data;
- 	uchar *greyPixels = (uchar*) imgray.data;
+	uchar *inPixels = (uchar*) imcolor->data;
+ 	uchar *greyPixels = (uchar*) imgray->data;
 	unsigned int brightness;
 
   	//for loops iterating thru the image pixel-by-pixel
-  	for (int rows = 0; rows<(imcolor.rows); rows++)
+  	for (int rows = (start/imcolor->cols); rows<((stop+1)/imcolor->cols); rows++)
 	{
-    		for (int cols = 0; cols<(imcolor.cols); cols++)
+    		for (int cols = 0; cols<(imcolor->cols); cols++)
 		{
 
       			brightness =
-			0.0722*inPixels[rows*imcolor.cols*3 + cols*3 + 0]   //B
-			+ 0.7152*inPixels[rows*imcolor.cols*3 + cols* 3 + 1]//G
-			+ 0.2126*inPixels[rows*imcolor.cols*3 + cols*3 + 2];//R
+			0.0722*inPixels[rows*imcolor->cols*3 + cols*3 + 0]   //B
+			+ 0.7152*inPixels[rows*imcolor->cols*3 + cols* 3 + 1]//G
+			+ 0.2126*inPixels[rows*imcolor->cols*3 + cols*3 + 2];//R
 
 
-      			greyPixels[rows*imcolor.cols + cols] = (uchar) brightness;
+      			greyPixels[rows*imcolor->cols + cols] = (uchar) brightness;
 		}
 	}
 	
-	return imgray;
+	return;
 }
 
-Mat to442_sobel(Mat imgray)
+void to442_sobel(Mat *imgray, Mat *imsobel, int start, int stop)
 {
 	/*
 	 * Gx		Gy
@@ -126,58 +171,63 @@ Mat to442_sobel(Mat imgray)
 	 * |G| = |Gx| + |Gy|
 	 */
 
-	//Define new matrix for sobel
-	Mat imsobel(imgray.rows-2, imgray.cols-2, CV_8UC1);
-
 	//using pixels from grayscale image, apply sobel
-	uchar *greyPixels = (uchar*) imgray.data;
-	uchar *sobelPixels = (uchar*) imsobel.data;
+	uchar *greyPixels = (uchar*) imgray->data;
+	uchar *sobelPixels = (uchar*) imsobel->data;
 	int gx, gy, g;
 
 	//for loops iterating thru the image pixel-by-pixel
-        for (int rows = 0; rows<(imsobel.rows); rows++)
+        for (int rows = (start/imsobel->cols); rows<((stop+1)/imsobel->cols); rows++)
         {
-                for (int cols = 0; cols<(imsobel.cols); cols++)
+                for (int cols = 0; cols<(imsobel->cols); cols++)
                 {
 
-                	gx = greyPixels[rows*imgray.cols+cols+2]
-			    -greyPixels[rows*imgray.cols+cols]
-			    +greyPixels[(rows+1)*imgray.cols+cols+2]*2
-			    -greyPixels[(rows+1)*imgray.cols+cols]*2
-			    +greyPixels[(rows+2)*imgray.cols+cols+2]
-			    -greyPixels[(rows+2)*imgray.cols+cols];
+                	gx = greyPixels[rows*imgray->cols+cols+2]
+			    -greyPixels[rows*imgray->cols+cols]
+			    +greyPixels[(rows+1)*imgray->cols+cols+2]*2
+			    -greyPixels[(rows+1)*imgray->cols+cols]*2
+			    +greyPixels[(rows+2)*imgray->cols+cols+2]
+			    -greyPixels[(rows+2)*imgray->cols+cols];
 
-			gy = greyPixels[rows*imgray.cols+cols+2]
-			    +greyPixels[rows*imgray.cols+cols+1]*2
-			    +greyPixels[rows*imgray.cols+cols+0]
-			    -greyPixels[(rows+2)*imgray.cols+cols+2]
-			    -greyPixels[(rows+2)*imgray.cols+cols+1]*2
-			    -greyPixels[(rows+2)*imgray.cols+cols+0];
+			gy = greyPixels[rows*imgray->cols+cols+2]
+			    +greyPixels[rows*imgray->cols+cols+1]*2
+			    +greyPixels[rows*imgray->cols+cols+0]
+			    -greyPixels[(rows+2)*imgray->cols+cols+2]
+			    -greyPixels[(rows+2)*imgray->cols+cols+1]*2
+			    -greyPixels[(rows+2)*imgray->cols+cols+0];
 			
 			g = (abs(gx)+abs(gy)) > 255 ? 255 : (abs(gx)+abs(gy));
-		       	sobelPixels[rows*imsobel.cols + cols] = (uchar) g;
+		       	sobelPixels[rows*imsobel->cols + cols] = (uchar) g;
                 }
         }
 
-        return imsobel;
+        return;
 
 }
 
-void groble(image, threadno)
+void *grobel(void *args)
 {
-    //convert to grayscale
-    //will prob need to separate definition of image from filling it with data
-    Mat grayImage = to442_grayscale(frame);
+    //initialize barrier for image processing threads
+    pthread_barrier_init(&threadBarrier, NULL, THREADCOUNT);
+    
+    //cast arguments passed in back to struct 
+    threadArgs temp = *((threadArgs*)args);
+    
+    //create pointer for grayscale image
+    Mat grayTemp(temp.input->rows, temp.input->cols, CV_8UC1);
 
-    //barrier
+    //convert to grayscale
+    to442_grayscale(temp.input, &grayTemp, temp.start1, temp.stop1);
+
+    //wait for threads to finish grayscale of image
+    pthread_barrier_wait(&threadBarrier);
     
     //apply sobel filter
-    //same deal, will prob need to separate definition from data filling
-    Mat sobelimage = to442_sobel(grayImage);
+    to442_sobel(&grayTemp, temp.output, temp.start2, temp.stop2);
 
-    //barrier 
+    //wait for threads to finish sobel filtering the image
+    pthread_barrier_wait(&threadBarrier);
 
-
-    return;
+    return NULL;
 }
 
