c19955e659c82d7eedc73d0bd057733e94798599
Nathan Jaggers <nathanj8910@gmail.com>
2022-11-10 21:21:23 -0800

    I was able to get mine to work now! There are areas to improve. Joshs worked because his sobel frame is the same as his others. Tilted images seem to come from having sobel smaller than the input from since it has two less rows and columns. Im sure there is a way to account for this with proper indexing but I couldn't find a solution at the moment. Time to go rockclimbing!

--- Lab5/lab5.cpp
+++ Lab5/lab5.cpp
@@ -1,218 +1,257 @@
-//Group: Nima Behmanesh, Nathan Jaggers, Joshua Rizzolo
-//Youtube Demo link: https://youtu.be/c75YS3goRbs
+/*****************************************************
+* Filename: lab5.cpp
+*
+* Description: brief description of file purpose
+*
+* Author: Nima Behmanesh, Nathan Jaggers, Josh Rizzolo
+******************************************************/
 
-#include<arm_neon.h>
-#include <stdio.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <pthread.h>
+//#include <arm_neon.h>
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
+//defines for any values you want to change easily
+#define THREADCOUNT 4
 
-void* grobel(void* input);
+//Create threads, and related pointers
+pthread_t thread[THREADCOUNT];
 
-//Prototyping threads
-pthread_t thread[4]; //4 Threads for 4
+void *threadStatus;
 
-//Barriers for synchronization
-pthread_barrier_t processingBarrier, frameGetBarrier;
+//create and initialize barriers for threads
+pthread_barrier_t grayBarrier, sobelBarrier;
 
-//arguments to be passed to child threads
-typedef struct threadArgs{
-  Mat *inputFrame;
-  Mat *outputFrame;
-  int height;
-  int width;
-  int startind;
-  int stopind;
-}ArgStruct;
+//create struct to hold arguments to pass to thread function
+struct threadArgs {
+    Mat *input;
+    Mat *grayTemp;
+    Mat *output;
+    int start1, stop1, start2, stop2;
+};
 
+//prototypes for functions
+void to442_grayscale(Mat*, Mat*, int, int);
+void to442_sobel(Mat*, Mat*, int, int, int);
+void *grobel(void *args);
 
+int main(int argc, char *argv[])
+{
+	// convert command line arg to string
+	if (!argv[1])
+	{
+		printf("No argument given to program.\n");
+		return -1;
+	}
 
+	string mediaName(argv[1]);
+	
+	// Read the video file
+	VideoCapture cap(mediaName);
 
-int main(){
+	// Check for failure
+	if (!cap.isOpened()) 
+ 	{
+		cout << "Could not open or find the file" << endl;
+		cin.get(); //wait for any key press
+ 		return -1;
+ 	}
+	
+	//Create Mats and read in one frame from video
+	Mat frame; 
+    cap.read(frame);
+    Mat grayimage(frame.rows, frame.cols, CV_8UC1);
+    Mat sobelimage(frame.rows, frame.cols, CV_8UC1);
 
-  //Loading vid and making a mat for each frame
-  VideoCapture cap("dab.mp4");
-  Mat frame;
+    //determine chunck size for threads
+    int graychunk = (frame.rows*frame.cols)/THREADCOUNT;
+    int sobelchunk = (sobelimage.rows*sobelimage.cols)/THREADCOUNT;
 
-  //Naming threads
-  int top, umid, lmid, bot, chunkSize, lastInd;
-
-  //If no vid can be loaded, exit main
-  if(!cap.isOpened()){
-    cout << "Error reading video file" << endl;
-    return -1;
-  }
-
-  //load one frame of video into the mat
-  cap >> frame;
-
-  //instantiating output frame of same size as input image but with only one color channel (and starting it as all black)
-  Mat grobelFrame = Mat(frame.rows, frame.cols, CV_8UC1, Scalar::all(0));
-  
-  //Getting data by which to split the mat into 4 chunks, one per thread
-  lastInd = frame.rows * frame.cols;
-  chunkSize = (int) lastInd/4;
-
-  //instantiating an argument struct for each thread with appropriate start and stop indices
-  //Sobel function requires padding above and below the center pixel, so give each thread two extra rows to greyscale so it finishes greyscaling everything needed for sobel
-  //without chunkSize - frame.cols and chunkSize + frame.cols sobel sees a hard black line below it's last center pixels
-  ArgStruct threadArgs0={.inputFrame = &frame, .outputFrame = &grobelFrame, .height = frame.rows, .width = frame.cols, .startind = 0, .stopind = chunkSize+frame.cols};
-  ArgStruct threadArgs1={.inputFrame = &frame, .outputFrame = &grobelFrame, .height = frame.rows, .width = frame.cols, .startind = chunkSize-frame.cols, .stopind = 2*chunkSize+frame.cols};
-  ArgStruct threadArgs2={.inputFrame = &frame, .outputFrame = &grobelFrame, .height = frame.rows, .width = frame.cols, .startind = 2*chunkSize-frame.cols, .stopind = 3*chunkSize+frame.cols};
-  ArgStruct threadArgs3={.inputFrame = &frame, .outputFrame = &grobelFrame, .height = frame.rows, .width = frame.cols, .startind = 3*chunkSize-frame.cols, .stopind = lastInd-1};
-
-
-  //instantiating barriers to wait for 5 threads: 4 child threads and main
-  pthread_barrier_init(&processingBarrier, NULL, 5); //Last arg is numThreads
-  pthread_barrier_init(&frameGetBarrier, NULL, 5);
-
- 
-  //Starting individual threads and passing pointers to their respective argument structures
-  top= pthread_create(&thread[0], NULL, grobel,  (void*) &threadArgs0);
+    //create and initialize arguments for threaded functions
+    struct threadArgs multi_t[THREADCOUNT]; 
     
-  umid = pthread_create(&thread[1], NULL, grobel,  (void*) &threadArgs1);
+    for(int i = 0; i < THREADCOUNT; i++)
+    {
+        multi_t[i].input  = &frame;
+        multi_t[i].grayTemp = &grayimage;
+        multi_t[i].output = &sobelimage;
+        multi_t[i].start1 = i*graychunk;
+        multi_t[i].stop1  = i*graychunk + graychunk - 1;
+        multi_t[i].start2 = i*sobelchunk;
+        multi_t[i].stop2  = i*sobelchunk + sobelchunk - 1;
+    }  
+
+   
+    //create barrier for main and threads
+    pthread_barrier_init(&sobelBarrier, NULL, THREADCOUNT + 1); 
+    pthread_barrier_init(&grayBarrier, NULL, THREADCOUNT);
     
-  lmid = pthread_create(&thread[2], NULL, grobel,  (void*) &threadArgs2);
+    //create persistent threads
+
+    for(int i = 0; i < THREADCOUNT; i++)
+    {
+        pthread_create(&thread[i], NULL, grobel, (void *)(&multi_t[i]));
+    }
+
+    //wait for processing of first frame
+    pthread_barrier_wait(&sobelBarrier);
+
+    while(cap.read(frame))
+    //while(true)
+	{
+            //grobel(multi_t);    
+
+            //after grabbing new frame, wait until processing is finished 
+            pthread_barrier_wait(&sobelBarrier);
+            
+            //cap >>frame;
+            //if(frame.empty()){break;}
+
+            //show final image
+        	imshow("Sobel", sobelimage);
+
+            //escape early if desired
+        	if(waitKey(30) == 27) // Wait for 'esc' key press to exit
+        	{ 
+            		break; 
+        	}
+
+            //wait for image to show and ensure esc is not pressed
+            //pthread_barrier_wait(&mainBarrier);
+	}
     
-  bot = pthread_create(&thread[3], NULL, grobel, (void*) &threadArgs3);
+    //barrier to let threads finish and exit 
+    pthread_barrier_wait(&sobelBarrier);
 
+    //Close the video file
+    cap.release();
   
-  
-  //Main loop loads frames into Mat, waits for child threads to finish processing,  gets next frame, and shows the finished previous frame
-  while(1){
+    //Close all image windows
+    destroyAllWindows();
 
-    
-    //Already have first frame, wait for it to finish processing
-    pthread_barrier_wait(&processingBarrier);
+    //Join threads
+    for(int i = 0; i < THREADCOUNT; i++)
+    {
+        pthread_join(thread[i], NULL);
+    }
 
-
-    //load new frame into mat and make sure it's not empty
-    cap >> frame;
-    if (frame.empty())
-      break;
-
-    //Show finished frame
-    imshow("Sobel Image", grobelFrame);
-    
-    //Wait for user to press esc, and close video when they do
-    char c = (char)waitKey(2);
-    if (c==27)
-      break;
-
-    //wait to work on new frame until the threads are all ready
-    pthread_barrier_wait(&frameGetBarrier);
-  }
-
-  //Close the video file
-  cap.release();
-  
-  //Close all image windows
-  destroyAllWindows();
-  
-  //Kill threads
-  pthread_join(top, NULL);
-  pthread_join(umid, NULL);
-  pthread_join(lmid, NULL);
-  pthread_join(bot, NULL);
-  //Finish Main
-  return 0;
-  
+	return 0;
 }
 
+void to442_grayscale(Mat *imcolor, Mat *imgray, int start, int stop)
+{
+
+	//using pixels from color image, generate grayscale
+	uchar *inPixels = (uchar*) imcolor->data + start*3;
+ 	uchar *greyPixels = (uchar*) imgray->data + start;
 
 
+    //define vectors for vector math and storing results
+    uint8x8_t w_r = vdup_n_u8(77);
+    uint8x8_t w_g = vdup_n_u8(150);
+    uint8x8_t w_b = vdup_n_u8(29);
+    uint8x8x3_t BGRvector;
+    uint16x8_t brightVector;
+    uint8x8_t result;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  	//for loops iterating thru the image pixel-by-pixel
+  	for (int index = start; index<(stop+1); index+=8, greyPixels+=8, inPixels+=(8*3))
+	{
+          //Reading color values into a structure of 3 vectors of 16 8-bit entries
+          //vld3 has a stride of 3, automatically separating entries into BGR
+	  BGRvector =  vld3_u8(inPixels);
+          //Turning BGR vector into brightness by adding them together post-scaling
+          brightVector = vmull_u8(BGRvector.val[0], w_r);
+          //multiply-accumulate-widen
+          brightVector = vmlal_u8(brightVector, BGRvector.val[1], w_g);
+          brightVector = vmlal_u8(brightVector, BGRvector.val[2], w_b);
+          result = vshrn_n_u16(brightVector, 8);
+          //Storing converted pixels in grey image
+          vst1_u8(greyPixels, result);
+	}
+}
 
+void to442_sobel(Mat *imgray, Mat *imsobel, int grayStart, int start, int stop)
+{
+	/*
+	 * Gx		Gy
+	 * [-1 +0 +1 	[+1 +2 +1
+	 *  -2 +0 +2 	 +0 +0 +0
+	 *  -1 +0 +1] 	 -1 -2 -1]
+	 *
+	 * |G| = |Gx| + |Gy|
+	 */
 
+	//using pixels from grayscale image, apply sobel
+	uchar *greyPixels = (uchar*) imgray->data + start;
+	uchar *sobelPixels = (uchar*) imsobel->data + start;
 
-//Greyscale + sobel function
-void* grobel(void* threadArgs){
+	int8x8_t topL, topM, topR, midL,  midR, botL, botM, botR;
+	uint16x8_t sresult;
+	uint16x8_t overflow = vdupq_n_u16(255);
 
-  //casting void * back to struct * to access struct elements again
-  ArgStruct *args = (ArgStruct *)threadArgs;
+	//for loops iterating thru the image pixel-by-pixel
+	for (int index = start; index<((stop+1)); index+=8, greyPixels+=8, sobelPixels+=8)
+        {
+		topL = vld1_u8((const uint8_t *)greyPixels);
+      		topM = vld1_u8((const uint8_t *)greyPixels+1);
+      		topR = vld1_u8((const uint8_t *)greyPixels+2);
 
-  //creating greyscale mat of same size as input image
-  Mat grey(args->height,args->width, CV_8UC1);
+      		midL = vld1_u8((const uint8_t *)(greyPixels+imgray->cols));
+      		midR = vld1_u8((const uint8_t *)(greyPixels+imgray->cols + 2));
 
-  int8x8_t topL, topM, topR, midL,  midR, botL, botM, botR;
-  uint8x8_t w_r = vdup_n_u8(77);
-  uint8x8_t w_g = vdup_n_u8(150);
-  uint8x8_t w_b = vdup_n_u8(29);
-  uint8x8x3_t BGRvector;
-  uint16x8_t brightVector;
-  uint8x8_t result;
-  uint16x8_t sresult;
-  uint16x8_t overflow = vdupq_n_u16(255);
-  
-  while(1){
-    
-    
-    //Getting pointers to Mat pixel data for input and grey images
-    const uchar *inPixels =  args->inputFrame->data + args->startind*3;
-    uchar *greyPixels = grey.data + args->startind;
-    uchar* sobelPixels = args->outputFrame->data + args->startind;
-    
-    //Iterate thru pixel data to last index, incrementing by length of vector
-    for(int index = args->startind; index<(args->stopind); index+=8, greyPixels+=8, inPixels+=8 * 3){
-      //Reading color values into a structure of 3 vectors of 16 8-bit entries
-      //vld3 has a stride of 3, automatically separating entries into BGR
-      BGRvector =  vld3_u8(inPixels);
-      //Turning BGR vector into brightness by adding them together post-scaling
-      brightVector = vmull_u8(BGRvector.val[0], w_r);
-      //multiply-accumulate-widen
-      brightVector = vmlal_u8(brightVector, BGRvector.val[1], w_g);
-      brightVector = vmlal_u8(brightVector, BGRvector.val[2], w_b);
-      result = vshrn_n_u16(brightVector, 8);
-      //Storing converted pixels in grey image
-      vst1_u8(greyPixels, result);
-			 				    
-    }
-    
-    
-    
-    //iterating thru 1D array of grey image pixel values to perform sobel on them
-    
-    greyPixels = grey.data + args->startind;
-    
-    for(int ind = args->startind + grey.cols;  ind<(args->stopind - grey.cols); greyPixels+=8, sobelPixels +=8, ind+=8){
+      		botL = vld1_u8((const uint8_t *)(greyPixels + 2*imgray->cols));
+      		botM = vld1_u8((const uint8_t *)(greyPixels + 2*imgray->cols+1));
+      		botR = vld1_u8((const uint8_t *)(greyPixels + 2*imgray->cols+2));
 
-      topL = vld1_u8((const uint8_t *)greyPixels);
-      topM = vld1_u8((const uint8_t *)greyPixels+1);
-      topR = vld1_u8((const uint8_t *)greyPixels+2);
+      		sresult = vaddq_u16(
+                        vabsq_s16(
+                                  vaddq_u16(
+                        vsubq_u16(vaddl_u8(topL, topR), vaddl_u8(botL, botR)),
+                        vsubq_u16(vshll_n_u8(topM, 1), vshll_n_u8(botM, 1)))),
+                         vabsq_s16(
+                                   vaddq_u16(
+                        vsubq_u16(vaddl_u8(topR, botR), vaddl_u8(topL, botL)),
+                        vsubq_u16(vshll_n_u8(midR, 1), vshll_n_u8(midL,1)))));
 
-      midL = vld1_u8((const uint8_t *)(greyPixels+grey.cols));
-      midR = vld1_u8((const uint8_t *)(greyPixels+grey.cols + 2));
-   
-      botL = vld1_u8((const uint8_t *)(greyPixels + 2*grey.cols));
-      botM = vld1_u8((const uint8_t *)(greyPixels + 2*grey.cols+1));
-      botR = vld1_u8((const uint8_t *)(greyPixels + 2*grey.cols+2));
-
-      sresult = vaddq_u16(
-			 vabsq_s16(
-				   vaddq_u16(
-			vsubq_u16(vaddl_u8(topL, topR), vaddl_u8(botL, botR)),
-			vsubq_u16(vshll_n_u8(topM, 1), vshll_n_u8(botM, 1)))), 
-			 vabsq_s16(
-				   vaddq_u16(
-			vsubq_u16(vaddl_u8(topR, botR), vaddl_u8(topL, botL)),
-			vsubq_u16(vshll_n_u8(midR, 1), vshll_n_u8(midL,1)))));   
-   
-   
-    
    sresult = vminq_u16(sresult, overflow);
-     
+
    vst1_u8(sobelPixels, vmovn_u16(sresult));
-   }
-    //wait here once done processing current frame before getting new frame
-    pthread_barrier_wait(&processingBarrier);
+
+	}
+
+        return;
+
+}
+
+void *grobel(void *args)
+{
+    //cast arguments passed in back to struct 
+    threadArgs temp = *((threadArgs*)args);
     
-    //wait here while getting the new frame so as not to access memory from multiple threads at once
-    pthread_barrier_wait(&frameGetBarrier);
-  }
-  return NULL;
+    while(!temp.input->empty())
+    {
+
+        //create pointer for grayscale image
+        //Mat grayTemp(temp.input->rows, temp.input->cols, CV_8UC1);
+
+        //convert to grayscale
+        to442_grayscale(temp.input, temp.grayTemp, temp.start1, temp.stop1);
+
+        //wait for threads to finish grayscale of image
+        pthread_barrier_wait(&grayBarrier);
+      
+        //apply sobel filter
+        to442_sobel(temp.grayTemp, temp.output, temp.start1, temp.start2, temp.stop2);
+
+        //wait for threads to finish sobel filtering the image
+        pthread_barrier_wait(&sobelBarrier);
+    
+    }
+
+    return NULL;
 }
 
